d99db5126692c78f20b5a29eb370fdafb17567d0
K.Kimura <dotnsf@gmail.com>
2023-06-06 09:58:30 +0900

    docker files added.

--- app.js
+++ app.js
@@ -75,13 +75,16 @@ app.get( '/api/model/:id', async function( req, res ){
 
 app.post( '/api/complete', async function( req, res ){
   res.contentType( 'application/json; charset=utf-8' );
-  var model = ( req.body.model ? req.body.model : 'text-davinci-003' );
+  var model = ( req.body.model ? req.body.model : 'gpt-3.5-turbo'/*'text-davinci-003'*/ );
   var max_tokens = ( req.body.max_tokens ? parseInt( req.body.max_tokens ) : 4000 );
   var prompt = req.body.prompt;
 
   var option = {
     model: model,
     prompt: prompt,
+    //messages: [
+    //  { role: "user", content: prompt }
+    //],
     max_tokens: max_tokens
   };
   if( req.body.temperature ){
@@ -94,18 +97,27 @@ app.post( '/api/complete', async function( req, res ){
     option.n = parseInt( req.body.n );
   }
 
-  var result = await openai.createCompletion( option );
-  var answer = result.data.choices[0].text;
+  try{
+    var result = await openai.createCompletion( option );
+    var answer = result.data.choices[0].text;
 
-  //. 最初の "\n\n" 以降が正しい回答？
-  var tmp = answer.split( "\n\n" );
-  if( tmp.length > 1 && tmp[0].length < IGNORE_PHRASE ){
-    tmp.shift();
-    answer = tmp.join( "\n\n" );
+    //. 最初の "\n\n" 以降が正しい回答？
+    var tmp = answer.split( "\n\n" );
+    if( tmp.length > 1 && tmp[0].length < IGNORE_PHRASE ){
+      tmp.shift();
+      answer = tmp.join( "\n\n" );
+    }
+
+    res.write( JSON.stringify( { status: true, result: answer }, null, 2 ) );
+    res.end();
+  }catch( err ){
+    console.log( err );
+    var status_code = ( err.response && err.response.status ? err.response.status : 400 );
+    var status_text = ( err.response && err.response.statusText ? err.response.statusText : 'unknown error' );
+    res.status( status_code )
+    res.write( JSON.stringify( { status: false, error: status_text }, null, 2 ) );
+    res.end();
   }
-
-  res.write( JSON.stringify( { status: true, result: answer }, null, 2 ) );
-  res.end();
 });
 
 app.post( '/api/image', async function( req, res ){
